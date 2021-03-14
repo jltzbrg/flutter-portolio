f0e5d51daf9823189f37c230f86550e2898b3237
Julio Litzenberg <julio.litzenberg@gmail.com>
2021-03-14 23:29:58 +0100

    Styling the Card Component

--- business_card_app/lib/main.dart
+++ business_card_app/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Business Card',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -29,7 +29,7 @@ class HomePage extends StatelessWidget {
       body: Container(
         alignment: Alignment.center,
         child: Stack(
-          alignment: Alignment.center,
+          alignment: Alignment.topCenter,
           children: [_getCard(), _getAvatar()],
         ),
       ),
@@ -41,18 +41,47 @@ class HomePage extends StatelessWidget {
     return Container(
       width: 350,
       height: 200,
+      margin: EdgeInsets.all(50.0),
       decoration: BoxDecoration(
-        color: Colors.amberAccent,
+        color: Colors.blueGrey.shade600,
         borderRadius: BorderRadius.circular(14.5),
+        boxShadow: [
+          BoxShadow(
+              color: Colors.grey,
+              offset: Offset(2, 3),
+              blurRadius: 0.5,
+              spreadRadius: 0.5)
+        ],
       ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Julio Litzenberg'),
-          Text('Flutter Developer'),
+          Text(
+            'Julio Litzenberg',
+            style: TextStyle(
+                fontSize: 20.9,
+                fontWeight: FontWeight.w500,
+                color: Colors.white),
+          ),
+          Text(
+            'Flutter Developer',
+            style: TextStyle(
+                fontSize: 17.0,
+                fontWeight: FontWeight.w300,
+                color: Colors.white),
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [Icon(Icons.person_outline), Text('Twitter: @jltzbrg')],
+            children: [
+              Icon(Icons.person_outline),
+              Text(
+                'Twitter: @jltzbrg',
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w200,
+                    color: Colors.white),
+              )
+            ],
           )
         ],
       ),
@@ -67,7 +96,7 @@ class HomePage extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.all(Radius.circular(50.0)),
-        border: Border.all(color: Colors.redAccent, width: 1.2),
+        border: Border.all(color: Colors.lightBlueAccent.shade100, width: 1.2),
         image: DecorationImage(
             image: NetworkImage('https://picsum.photos/300/300'),
             fit: BoxFit.cover),
